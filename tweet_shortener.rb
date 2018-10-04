a27aa90685be7387f947a3d774401cbad05cdf2e
Min Hee  Park <minheekangg@gmail.com>
2018-10-04 18:22:31 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -70,9 +70,20 @@ def selective_tweet_shortener(tweets)
 
 if tweets.length > 140
 
-
-
-
+    tweets.each do |string|
+      new_tweets = ""
+      array = string.split(" ").to_a
+      new_array = []
+        array.each do |word|
+          if dictionary.has_key?(word.downcase)
+            new_array << dictionary.fetch(word.downcase)
+          else
+            new_array << word
+          end
+        end
+        new_tweets << new_array.join(" ")
+    puts new_tweets
+    end
 
 else
   return tweets
