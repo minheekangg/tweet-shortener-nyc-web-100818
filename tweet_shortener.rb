025536b5116322987c22badd706419d2c7d7690a
Min Hee  Park <minheekangg@gmail.com>
2018-10-04 17:58:46 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,52 @@
-# Write your code here.
+
+def word_substituter(string)
+  dictionary = {
+    "hello"=> "hi",
+    "two"=> 2,
+    "to"=> 2,
+    "too"=> 2,
+    "for"=> 4,
+    "four"=> 4,
+    "be"=> "b",
+    "you"=> "u",
+    "at"=> "@",
+    "and"=> "&"
+  }
+  array = string.split(" ").to_a
+  new_array = []
+    array.each do |word|
+      if dictionary.has_key?(word)
+        new_array << dictionary.fetch(word)
+      else
+        new_array << word
+      end
+    end
+return new_array.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  dictionary = {
+    "hello"=> "hi",
+    "two"=> 2,
+    "to"=> 2,
+    "too"=> 2,
+    "for"=> 4,
+    "four"=> 4,
+    "be"=> "b",
+    "you"=> "u",
+    "at"=> "@",
+    "and"=> "&"
+  }
+tweets.each do |string|
+  array = string.split(" ").to_a
+  new_array = []
+    array.each do |word|
+      if dictionary.has_key?(word)
+        new_array << dictionary.fetch(word)
+      else
+        new_array << word
+      end
+    end
+return new_array.join(" ")
+end
+end
