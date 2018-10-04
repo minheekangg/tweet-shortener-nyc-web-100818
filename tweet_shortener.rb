3a45765b65c60f763d93fcfaab6e64a25a1ac682
Min Hee  Park <minheekangg@gmail.com>
2018-10-04 18:29:36 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -88,9 +88,10 @@ end
 def shortened_tweet_truncator(tweet)
   shortened_tweet = ""
   truncated_tweet = ""
+
 if tweet.length > 140
   truncated_tweet = selective_tweet_shortener(tweet)
-    if truncated_tweet >=140
+    if truncated_tweet >140
       shortened_tweet = truncated_tweet[141..-1].delete
       return truncated_tweet
     else
